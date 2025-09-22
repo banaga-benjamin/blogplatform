08e3a4656cca48f459a850f55b186ad6559a4213
benjamin <banaga.benjaminarlantico@gmail.com>
2025-09-22 16:35:01 +0800

    feat(authentication): added dtos for authentication
    - implemented AuthRequest as DTO for register and login requests
    - implemented AuthResponse as DTO for holding JWT token after login

---
+++ src/main/java/com/example/blogplatform/dtos/AuthRequest.java
@@ -0,0 +1,22 @@
+package com.example.blogplatform.dtos;
+
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotNull;
+
+public class AuthRequest {
+
+    @NotNull
+    @Size(min = 5, max = 20, message = "username must be between one and twenty characters long")
+    private String username;
+
+    @NotNull
+    @Size(min = 5, max = 30, message = "password must be between five and thirty characters long")
+    private String password;
+
+    public String getUserName( ) { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword( ) { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+}
