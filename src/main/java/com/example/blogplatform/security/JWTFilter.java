780bb10e8056760a79d8a4addfec31e902551bbc
benjamin <banaga.benjaminarlantico@gmail.com>
2025-09-22 18:08:29 +0800

    feat(security): implemented login via JWT
    - added request filter for validation of JWT (if present)
    - non-auth endpoints are accessible only with valid token

---
+++ src/main/java/com/example/blogplatform/security/JWTFilter.java
@@ -0,0 +1,57 @@
+package com.example.blogplatform.security;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+public class JWTFilter extends OncePerRequestFilter {
+    private final JWTUtil util;
+    private final UserDetailsService service;
+
+    public JWTFilter(UserDetailsService service, JWTUtil util) {
+        this.util = util;
+        this.service = service;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filter)
+            throws ServletException, IOException {
+        
+        String path = request.getServletPath( );
+        if (path.startsWith("/apis/auth")) {
+            filter.doFilter(request, response);
+            return;
+        }
+
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filter.doFilter(request, response);
+            return;
+        }
+        
+        String token = authHeader.substring(7);
+        String username = util.extractUsername(token);
+        if (username == null) {
+            filter.doFilter(request, response);
+            return;
+        }
+
+        UserDetails user = service.loadUserByUsername(username);
+        if (util.validateToken(token, user.getUsername( ))) {
+
+            UsernamePasswordAuthenticationToken authToken =
+                    new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities( ));
+            SecurityContextHolder.getContext( ).setAuthentication(authToken);
+        }
+        filter.doFilter(request, response);
+    }
+}
