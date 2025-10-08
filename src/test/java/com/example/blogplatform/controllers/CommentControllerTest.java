dd0e7cfe5ee14d437ed9d0f3c4e723f081a637d0
benjamin <banaga.benjaminarlantico@gmail.com>
2025-10-08 09:46:02 +0800

    test(comment): added unhappy path integration tests for comment update endpoint
    - additionally fixed bug in existing update endpoint integration test

--- src/test/java/com/example/blogplatform/controllers/CommentControllerTest.java
+++ src/test/java/com/example/blogplatform/controllers/CommentControllerTest.java
@@ -129,6 +129,7 @@ public class CommentControllerTest {
         Long id = mapper.readValue(result_json, CommentDTO.class).getId( );
 
         request.setContent("updated comment content");
+        request_json = mapper.writeValueAsString(request);
 
         mock.perform(put("/apis/comment/{id}", id)
                         .header("Authorization", "Bearer " + token)
@@ -136,6 +137,47 @@ public class CommentControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(request_json))
             .andExpect(status( ).isOk( ));
+    }
+
+    @Test
+    public void updateNonExistentComment( ) throws Exception {
+        // nonexistent post and comment
+        CommentRequest request = new CommentRequest( );
+        request.setContent("example comment content");
+        String request_json = mapper.writeValueAsString(request);
+
+        mock.perform(put("/apis/comment/{id}", 9999)
+                        .header("Authorization", "Bearer " + token)
+                        .queryParam("pid", String.valueOf(9999))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(request_json))
+            .andExpect(status( ).isNotFound( ));
+
+        // existent post but nonexistent comment
+        PostRequest postRequest = new PostRequest( );
+        postRequest.setContent("example post content");
+        postRequest.setTitle("example post title");
+        String postrequest_json = mapper.writeValueAsString(postRequest);
+
+        MvcResult postresult = mock.perform(post("/apis/post")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(postrequest_json))
+            .andReturn( );
+        
+        String postresult_json = postresult.getResponse( ).getContentAsString( );
+        Long pid = mapper.readValue(postresult_json, PostDTO.class).getId( );
+
+        request = new CommentRequest( );
+        request.setContent("example comment content");
+        request_json = mapper.writeValueAsString(request);
+
+        mock.perform(put("/apis/comment/{id}", 9999)
+                        .header("Authorization", "Bearer " + token)
+                        .queryParam("pid", String.valueOf(pid))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(request_json))
+            .andExpect(status( ).isNotFound( ));
     }
 
     // delete comment test
